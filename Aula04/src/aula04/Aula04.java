45a6309b78dd3017c5b66ef3f758ab2866e29901
andre <lbarros.andre@gmail.com>
2022-03-06 20:57:07 -0300

    projeto_caneta_java

---
+++ Aula04/src/aula04/Aula04.java
@@ -0,0 +1,11 @@
+
+package aula04;
+public class Aula04 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("NIC", "Amarelo", 0.3f);
+        c1.status();   
+        Caneta c2 = new Caneta("PEP", "Lustrada", 3.1f);
+        c2.status();
+    }
+    
+}
